bcf61e3757b3c45f366561c0e71b1711f147eab2
antoniomarcelino <antoniomarcelinodev@gmail.com>
2022-11-17 08:31:18 -0300

    Criando componentes

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
+import Formulario from './Formulario';
+import Tabela from './Tabela';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Formulario />
+      <Tabela />
     </div>
   );
 }
