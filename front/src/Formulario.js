64860ed0db9b9b3bd170b110d092ccfed4647514
antoniomarcelino <antoniomarcelinodev@gmail.com>
2022-11-17 08:59:54 -0300

    Estilizando componentes

--- front/src/Formulario.js
+++ front/src/Formulario.js
@@ -1,14 +1,14 @@
 function Formulario(){
     return(
-        <div>
-            <input type='text' placeholder='Nome' />
-            <input type='text' placeholder='Marca' />
+        <form>
+            <input type='text' placeholder='Nome' className='form-control'/>
+            <input type='text' placeholder='Marca' className='form-control'/>
     
-            <input type='button' value='Cadastrar' />
-            <input type='button' value='Remover' />
-            <input type='button' value='Alterar' />
-            <input type='button' value='Cancelar' />
-        </div>
+            <input type='button' value='Cadastrar' className='btn btn-primary' />
+            <input type='button' value='Remover' className='btn btn-danger' />
+            <input type='button' value='Alterar' className='btn btn-warning' />
+            <input type='button' value='Cancelar' className='btn btn-secondary' />
+        </form>
     )
 }
 
--- front/src/Tabela.js
+++ front/src/Tabela.js
@@ -1,6 +1,6 @@
 function Tabela(){
     return(
-        <table>
+        <table className='table'>
             <thead>
                 <tr>
                     <th>#</th>
@@ -10,10 +10,12 @@ function Tabela(){
                 </tr>
             </thead>
             <tbody>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <tr>
+                    <td>a</td>
+                    <td>b</td>
+                    <td>c</td>
+                    <td>d</td>
+                </tr>
             </tbody>
         </table>
     )
