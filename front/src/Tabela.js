9a4a3c42e76a215547ca0b40079ea0c20a1080e7
antoniomarcelino <antoniomarcelinodev@gmail.com>
2022-11-17 08:43:12 -0300

    Estrutura da tabela

--- front/src/Tabela.js
+++ front/src/Tabela.js
@@ -1,6 +1,21 @@
 function Tabela(){
     return(
-        <h1>Tabela</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Nome</th>
+                    <th>Marca</th>
+                    <th>Selecionar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tbody>
+        </table>
     )
 }
 
